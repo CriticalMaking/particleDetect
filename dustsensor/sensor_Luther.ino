d235af63cbbabb74b261e59b282c54c6f5833371
Zhen Qin <z.qin@mail.utoronto.ca>
2020-05-25 14:32:18 -0400

    Add files via upload

---
+++ dustsensor/sensor_Luther.ino
@@ -0,0 +1,51 @@
+//Luther's version
+//Notice: The hardware settings may be different
+
+int measurePin = A5;
+int ledPower = 14;
+ 
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+ 
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+ 
+void setup(){
+  Serial.begin(9600);
+  pinMode(ledPower,OUTPUT);
+}
+ 
+void loop(){
+  digitalWrite(ledPower,LOW); // power on the LED
+  delayMicroseconds(samplingTime);
+ 
+  voMeasured = analogRead(measurePin); // read the dust value
+ 
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH); // turn the LED off
+  delayMicroseconds(sleepTime);
+ 
+  // 0 - 3.3V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (5 / 4096.0);
+ 
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = 0.172 * calcVoltage - 0.0999;
+  
+  //convert the unit from mg/m^3 to ug/m^3
+  dustDensity = dustDensity * 1000;
+ 
+  Serial.print("Raw Signal Value (0-1023): ");
+  Serial.print(voMeasured);
+ 
+  Serial.print(" - Voltage: ");
+  Serial.print(calcVoltage);
+ 
+  Serial.print(" - Dust Density (ug/m^3): ");
+  Serial.println(dustDensity);
+ 
+  delay(1000);
+}
